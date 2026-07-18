aa7a2497fa3b9b9d50b0a4b9591daccdbf1be7a7
João Martiniano <44736477+joaomartiniano@users.noreply.github.com>
2026-07-18 11:45:25 +0100

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_Ficha1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercício 1: declare e inicialize um array do tipo int chamado numeros com os valores 1, 2, 3, 4 e 5
+
+            // *** Inserir resolução do exercício 1 aqui
+
+            // Exercício 2: declare e inicialize um array chamado diasSemana que guarda os nomes dos dias da semana
+
+            // *** Inserir resolução do exercício 2 aqui
+
+            // Exercício 3: declare e inicialize um array chamado temperaturas (do tipo double) com os seguintes valores: 23.5, 25.0, 22.8, 18.2, 17.0
+
+            // *** Inserir resolução do exercício 3 aqui
+
+            // Exercício 4: declare e inicialize um array chamado letras (do tipo char) com os seguintes valores: 'a', 'b', 'c', 'd', 'e'
+
+            // *** Inserir resolução do exercício 4 aqui
+
+            // Exercício 5: altere o 1º elemento do array numeros: atribua o valor 99
+
+            // *** Inserir resolução do exercício 5 aqui
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Arrays - Ficha de trabalho 1
+
+Neste repositório encontra 5 exercícios.
+
+Resolva cada exercício, inserindo o código na linha com comentário ```// *** Inserir resolução do exercício aqui```
+
+Exemplo:
+
+```csharp
+// Exercício 1: declare e inicialize um array do tipo int chamado numeros com os valores 1, 2, 3, 4 e 5
+
+int[] numeros = { 1, 2, 3, 4, 5 };
+```
+
+Ao terminar cada exercício faça commit com a mensagem ```Resolução exercício x``` (em que ```x``` é o número do exercício que resolveu)
